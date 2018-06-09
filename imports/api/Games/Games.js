8dbe2513dd5e7172b9aa2301300642d0405c3318
Daniel Lewis <daniel@pasme.com.au>
2018-06-09 21:48:43 +1000

    Update Games.js

--- imports/api/Games/Games.js
+++ imports/api/Games/Games.js
@@ -43,13 +43,18 @@ Games.schema = new SimpleSchema({
     type: String,
     label: 'The title of the game.',
   },
+  altTitle: {
+    type: String,
+    label: 'Alternate names for this game.',
+    optional: true,
+  },
   description: {
     type: String,
     label: 'An extract of the game.',
   },
   wishlist: {
     type: Array,
-    label: 'People who want this game',
+    label: 'People who want this game.',
     optional: true,
   },
   'wishlist.$': {
@@ -57,22 +62,101 @@ Games.schema = new SimpleSchema({
   },
   owns: {
     type: Array,
-    label: 'People who own this game',
+    label: 'People who own this game.',
     optional: true,
   },
   'owns.$': {
     type: String,
   },
-  altTitle: {
-    type: String,
-    label: 'Alternate names for this games.',
-    optional: true,
-  },
   rrp: {
     type: String,
-    label: 'Recomended Retail Price for this games.',
+    label: 'Recomended Retail Price for this game.',
     optional: true,
   },
+  edition: {
+		type: String,
+		label: 'The edition number of this game.',
+		optional: true,
+	},
+	pubYear: {
+		type: String,
+		label: 'The year this game/edition was published.',
+		optional: true,
+	},
+	players: {
+		type: String,
+		label: 'The number of players that play this game.',
+		optional: true,
+	},
+	playTime: {
+		type: String,
+		label: 'How long is the advised playing time for this game, in minutes.',
+		optional: true,
+	},
+	ageRange: {
+		type: String,
+		label: 'What is the recomended age range for this game.',
+		optional: true,
+	},
+	designers: {
+		type: String,
+		label: 'Who were the designers of this game.',
+		optional: true,
+	},
+	artist: {
+		type: String,
+		label: 'Who were the artists on this game.',
+		optional: true,
+	},
+	publishers: {
+		type: String,
+		label: 'Who were the publishers of this game/edition.',
+		optional: true,
+	},
+	categories: {
+		type: Array,
+		label: 'What categories does this game fit into.',
+		optional: true,
+	},
+	'categories.$': {
+		type: String,
+	},
+	gameMech: {
+		type: Array,
+		label: 'What game mechanics does this game utilise.',
+		optional: true,
+	},
+	'gameMech.$': {
+		type: String,
+	},
+	type: {
+		type: Array,
+		label: 'What type of game is this.',
+		optional: true,
+	},
+	'type.$': {
+		type: String,
+	},
+	kidFriendly: {
+		type: Boolean,
+		label: 'Would kids be able to play this game.',
+		optional: true,
+	},
+	expansion: {
+		type: Boolean,
+		label: 'Is this an expansion of another game.',
+		optional: true,
+	},
+	standalone: {
+		type: Boolean,
+		label: 'If this is an expansion, does it require the base game.',
+		optional: true,
+	},
+	expAvail: {
+		type: Boolean,
+		label: 'Is there an expansion available for this game.',
+		optional: true,
+	},
 });
 
 Games.attachSchema(Games.schema);
