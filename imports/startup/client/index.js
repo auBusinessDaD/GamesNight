ec9c8c402b9e7e8996fc4027abe10010ecacf7be
Daniel Lewis <daniel@pasme.com.au>
2018-07-02 21:38:24 +1000

    wip wiring up admin panel for managing user settings

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -27,23 +27,19 @@ injectGlobal`
     --info: #5bc0de;
     --warning: #f0ad4e;
     --danger: #d9534f;
-
     --gray-darker: #222;
     --gray-dark: #333;
     --gray: #555;
     --gray-light: #777;
     --gray-lighter: #eee;
-
     --facebook: #3b5998;
     --google: #ea4335;
     --github: var(--gray-dark);
-
     --cb-blue: #4285F4;
     --cb-green: #00D490;
     --cb-yellow: #FFCF50;
     --cb-red: #DA5847;
   }
-
   html {
     position: relative;
     min-height: 100%;
@@ -56,23 +52,19 @@ injectGlobal`
     font-size: 14px;
     line-height: 20px;
   }
-
   .navbar {
     border-radius: 0;
     border-left: none;
     border-right: none;
     border-top: none;
   }
-
   form label {
     display: block;
   }
-
   form .control-label {
     display: block;
     margin-bottom: 7px;
   }
-
   form label.error {
     display: block;
     margin-top: 8px;
@@ -80,11 +72,12 @@ injectGlobal`
     font-weight: normal;
     color: var(--danger);
   }
-
   .page-header {
     margin-top: 0;
   }
-
+  .table tr td {
+    vertical-align: middle !important;
+  }
   @media screen and (min-width: 768px) {
     .page-header {
       margin-top: 20px;
