a943fb3cd9fdf43aae60bc1b356fc332a66b55d8
Daniel Lewis <daniel@pasme.com.au>
2018-07-04 21:35:09 +1000

    Update Loading.js

--- imports/ui/components/Loading/Loading.js
+++ imports/ui/components/Loading/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => (
   <div className="Loading">
-    <img src="https://cdn.dribbble.com/users/563824/screenshots/3633228/untitled-5.gif" />
+    <img src="/loading.gif" />
   </div>
 );
 
