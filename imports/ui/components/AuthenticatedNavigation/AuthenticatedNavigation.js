32f1a0f01c80936eb28c50b6d99282e49b219bcd
Daniel Lewis <daniel@pasme.com.au>
2018-08-12 03:02:12 +1000

    Update AuthenticatedNavigation.js

--- imports/ui/components/AuthenticatedNavigation/AuthenticatedNavigation.js
+++ imports/ui/components/AuthenticatedNavigation/AuthenticatedNavigation.js
@@ -11,36 +11,39 @@ const AuthenticatedNavigation = ({ name, history, userId }) => (
       <LinkContainer to="/games">
         <NavItem eventKey={1} href="/games">All Games</NavItem>
       </LinkContainer>
+      <LinkContainer to="/playlist">
+        <NavItem eventKey={2} href="/playlist">My Itchlist</NavItem>
+      </LinkContainer>
       <LinkContainer to="/wishlist">
-        <NavItem eventKey={2} href="/wishlist">My Wishlist</NavItem>
+        <NavItem eventKey={3} href="/wishlist">My Wishlist</NavItem>
       </LinkContainer>
       <LinkContainer to="/mine">
-        <NavItem eventKey={3} href="/mine">My Games Shelf</NavItem>
+        <NavItem eventKey={4} href="/mine">My Games Shelf</NavItem>
       </LinkContainer>
-      {Roles.userIsInRole(userId, 'publisher') ? <NavDropdown eventKey={4} title="Publisher" id="publisher-nav-dropdown">
+      {Roles.userIsInRole(userId, 'publisher') ? <NavDropdown eventKey={5} title="Publisher" id="publisher-nav-dropdown">
         <LinkContainer to="/game/manage">
-          <NavItem eventKey={4.1} href="/game/manage">Manage Games</NavItem>
+          <NavItem eventKey={5.1} href="/game/manage">Manage Games</NavItem>
         </LinkContainer>
         <LinkContainer exact to="/game/manage/new">
-          <NavItem eventKey={4.2} href="/game/manage/new">Add Game</NavItem>
+          <NavItem eventKey={5.2} href="/game/manage/new">Add Game</NavItem>
         </LinkContainer>
       </NavDropdown> : ''}
-      {Roles.userIsInRole(userId, 'admin') ? <NavDropdown eventKey={5} title="Admin" id="admin-nav-dropdown">
+      {Roles.userIsInRole(userId, 'admin') ? <NavDropdown eventKey={6} title="Admin" id="admin-nav-dropdown">
         <LinkContainer exact to="/admin/users">
-          <NavItem eventKey={5.1} href="/admin/users">Users</NavItem>
+          <NavItem eventKey={6.1} href="/admin/users">Users</NavItem>
         </LinkContainer>
         <LinkContainer exact to="/admin/users/settings">
-          <NavItem eventKey={5.2} href="/admin/users/settings">User Settings</NavItem>
+          <NavItem eventKey={6.2} href="/admin/users/settings">User Settings</NavItem>
         </LinkContainer>
       </NavDropdown> : ''}
     </Nav>
     <Nav pullRight>
-      <NavDropdown eventKey={6} title={name} id="user-nav-dropdown">
+      <NavDropdown eventKey={7} title={name} id="user-nav-dropdown">
         <LinkContainer to="/profile">
-          <NavItem eventKey={6.1} href="/profile">Profile</NavItem>
+          <NavItem eventKey={7.1} href="/profile">Profile</NavItem>
         </LinkContainer>
         <MenuItem divider />
-        <MenuItem eventKey={6.2} onClick={() => history.push('/logout')}>Logout</MenuItem>
+        <MenuItem eventKey={7.2} onClick={() => history.push('/logout')}>Logout</MenuItem>
       </NavDropdown>
     </Nav>
   </div>
